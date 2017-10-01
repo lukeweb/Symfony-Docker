76129ae6acf703520dbbe61b873b45b8c5314383
Łukasz Pstrowski <lukasz.pstrowski@gmail.com>
2017-10-01 12:43:30 +0200

    Created basic Dockerfiles for nginx, php-fpm, added docker-compose file, script to run after containers are up

---
+++ php7-fpm/scripts/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# change ownership and permissions for symfony cache directory
+chown -R www-data:www-data /var/www/symfony.dev/var
+chmod 755 /var/www/symfony.dev/var
+
+# clear caches symfony 3.*
+/var/www/symfony.dev/bin/console cache:clear
+
+# keep PHP-FPM process as a main process
+php-fpm -F
