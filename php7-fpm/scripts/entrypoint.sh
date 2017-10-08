31464a10ec297ec4c0d042e2f1216949c8c5000e
Łukasz Pstrowski <lukasz.pstrowski@gmail.com>
2017-10-08 22:19:09 +0200

    Changes do docker-compose.yml file, adding no wormup cache to entrypoint.sh for fixing permissions problem, creating simple build for travis - created filr travis.yml

--- php7-fpm/scripts/entrypoint.sh
+++ php7-fpm/scripts/entrypoint.sh
@@ -5,7 +5,7 @@ chown -R www-data:www-data /var/www/symfony.dev/var
 chmod 755 /var/www/symfony.dev/var
 
 # clear caches symfony 3.*
-/var/www/symfony.dev/bin/console cache:clear
+/var/www/symfony.dev/bin/console cache:clear --no-warmup
 
 # keep PHP-FPM process as a main process
 php-fpm -F
